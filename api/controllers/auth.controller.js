9dbecd5593aa394b4faed02980c130ff1ddae026
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-06-11 13:38:15 +0530

    add sign up UI to api routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,9 +4,11 @@ import { errorHandle } from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
+    
     if(!username || !email || !password){
-        return res.status(404).json("invalid information");
+        return next(errorHandle(404, "invalid information"));
     }
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new userModel({username, email, password: hashedPassword});
     try{
@@ -15,4 +17,4 @@ export const signUp = async (req, res, next) => {
     }catch(error){
         next(error);
     }
-}
+};
--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  console.log(formData);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      this is signUp page
+    <div className='flex justify-center items-center h-[90vh] bg-[#C5FFF8]'>
+      <div className="w-[28rem] p-4 rounded-3xl shadow-2xl bg-white">
+        <h1 className='text-center text-3xl my-7'>Sign Up</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center my-4 p-4 gap-4">
+          <input type="text" placeholder='Username' id='username' className='px-4 py-3 rounded-lg  border-2 border-gray-300' autoComplete='off' onChange={handleChange} />
+          <input type="email" placeholder='email' id='email' className='px-4 py-3 rounded-lg  border-2 border-gray-300' autoComplete='off' onChange={handleChange} />
+          <input type="password" placeholder='password' id='password' className='px-4 py-3 rounded-lg border-2 border-gray-300' autoComplete='off' onChange={handleChange} />
+          <button disabled={loading} className="bg-blue-600 text-xl text-white font-semibold w-full py-2 rounded-lg transition-all duration-300 hover:bg-blue-500 disabled:bg-blue-400">{loading ? 'Loading...' : 'Sign Up'}</button>
+        </form>
+        <p className='px-6 text-xs font-semibold my-4'>By continuing, you agree to NovaMart's Terms of Service and acknowledge you've read our Privacy Policy.Notice all collection</p>
+        <div className="flex gap-2 px-4 my-8">
+          <p>Already have an account ?</p>
+          <Link to='/sign-in' className='text-blue-500 font-semibold'>Login</Link>
+        </div>
+        {error && (
+          <p className="text-red-600 text-sm font-semibold px-4 text-center">{error}</p>
+        )}
+      </div>
     </div>
   )
 }
