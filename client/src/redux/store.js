6d281eb12c38a2d1c285f8635fdee612f12c57a6
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-06-11 19:39:25 +0530

    add react resux and connect sign in UI to sign in api route

---
+++ client/src/redux/store.js
@@ -0,0 +1,25 @@
+import {combineReducers ,configureStore } from '@reduxjs/toolkit'
+import userReducer from "./user/userSlice";
+import {persistStore, persistReducer} from "redux-persist";
+import sessionStorage from 'redux-persist/lib/storage/session';
+
+
+const rootReducer = combineReducers({user: userReducer});
+
+const persistConfig = {
+    key: 'root',
+    storage: sessionStorage,
+    version: 1,
+  };
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
