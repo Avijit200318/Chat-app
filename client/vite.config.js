aade4701c2165ac744ad0ef80d4c4c69bb98f6ec
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-31 19:18:36 +0530

    update normal website to a PWA website

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,60 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+        name: 'ChatPlus',
+        short_name: 'ChatPlus',
+        description: 'This is a messaging and file sharing website',
+        icons: [
+          {
+            src: '/images/logo144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '.',
+        theme_color: '#fff',
+        background_color: '#fff'
+      },
+      workbox: {
+        runtimeCaching: [{
+          urlPattern: ({ url }) => {
+            return url.pathname.startsWith("/api");
+          },
+          handler: "CacheFirst",
+          options: {
+            cacheName: "api-cache",
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }]
+      },
+      registerType: 'autoUpdate'
+    })
+  ],
   build: {
     chunkSizeWarningLimit: 1000
   }
